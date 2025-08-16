44874ab551dbf21d0844f7ef000cfd072167ade8
Nathan Watterson <nathanw@huma.us>
2025-08-15 23:20:10 -0700

    Quartz sync: Aug 15, 2025, 11:20 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -72,7 +72,6 @@ const config: QuartzConfig = {
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
-      Plugin.Excalidraw(),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
